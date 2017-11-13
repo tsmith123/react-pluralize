dab818668d33ef874b0047933913780491a778a4
Thomas Smith <tsmith123@users.noreply.github.com>
2017-11-13 14:18:44 +0000

    add showCount feature

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ This tiny React component makes it really easy to pluralize words. It doesn't re
     <Pluralize singular="view" count={3} /> => 3 views
     <Pluralize singular="sheep" plural="sheep" count={3} /> => 3 sheep
     <Pluralize singular="like" /> => 1 like
+    <Pluralize singular="like" showCount={false}/> => like
 
     // Dyanmic Updating
     // If count prop is provided with a value that changes during runtime then the output changes accordingly
@@ -23,12 +24,15 @@ This tiny React component makes it really easy to pluralize words. It doesn't re
 
 ## Props
 
-**singular** (required)
+**singular** (required)(String)
 Provide the singular form of the noun
 
-**plural** (optional)
+**plural** (optional)(String)
 In the English language most nouns are pluralized by adding an "s" to the end but if this isn't the case then you need to provide the irregular plural form as a prop value. For example, if the noun (singular) is "person" then provide "people" as the value.
 
-**count** (optional but recommended)
+**count** (optional but recommended)(default: 1)(Number)
 If you don't provide a count prop value then a default value of 1 is used.
 To take advantage of dynamic updating, you should include this prop.
+
+**showCount** (optional)(default: true)(Boolean)
+If you would prefer not to see the count in the output then set this prop to false.
--- index.js
+++ index.js
@@ -2,56 +2,59 @@
 React Pluralize (MIT)
 */
 
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class Pluralize extends React.Component {
-  static displayName = 'Pluralize'
+  static displayName = 'Pluralize';
   static defaultProps = {
     count: 1,
+    showCount: true,
   }
 
   constructor() {
-    super()
+    super();
 
     this.state = {
-      text: ""
+      output: ""
     }
   }
 
   componentDidMount () {
-    this.pluralize(this.props)
+    this.pluralize(this.props);
   }
 
   // update text if count prop changes
   componentWillReceiveProps(nextProps) {
     if(nextProps.count !== this.props.count) {
-      this.pluralize(nextProps)
+      this.pluralize(nextProps);
     }
   }
 
   pluralize(data) {
     let text = "";
-    let { singular, plural, count } = data
+    let { singular, plural, count, showCount } = data;
 
     // if not specified then use standard plural ending
     if(!plural) {
-      plural = singular + "s"
+      plural = singular + "s";
     }
 
     // set correct ending to text
     count === 1 ? text = singular : text = plural
+    if(showCount) {
+      text = String(count) + " " + text;
+    }
 
     // update state
     this.setState({text: text})
   }
 
   render() {
-    const { count } = this.props
     const { text } = this.state
 
     return (
-      <span>{count} {text}</span>
+      <span>{text}</span>
     )
   }
 }
@@ -60,4 +63,5 @@ Pluralize.propTypes = {
   singular: PropTypes.string.isRequired,
   plural: PropTypes.string,
   count: PropTypes.number,
+  showCount: PropTypes.bool,
 }
