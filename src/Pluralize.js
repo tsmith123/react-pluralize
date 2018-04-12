07b53e2366a7f10757a17968b3016cb66dc3f92d
Thomas Smith <tsmith123@users.noreply.github.com>
2018-04-12 18:54:29 +0100

    update webpack config

---
+++ src/Pluralize.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Pluralize extends Component {
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return (
+      
+    )
+  }
+}
+
+export default Pluralize;
