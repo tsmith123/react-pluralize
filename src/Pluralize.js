088d731be0fe6672941ae2d6632f596cac91a54d
Thomas Smith <tsmith123@users.noreply.github.com>
2018-04-13 15:18:15 +0100

    add transform class properties plugin

--- src/Pluralize.js
+++ src/Pluralize.js
@@ -1,14 +1,55 @@
+/*
+ * React Pluralize (MIT)
+ * Author: Tom Smith (https://github.com/tsmith123)
+ */
+
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Pluralize extends Component {
-  componentDidMount() {
-    
+  static propTypes = {
+    singular: PropTypes.string.isRequired,
+    plural: PropTypes.string,
+    count: PropTypes.number,
+    showCount: PropTypes.bool,
+    className: PropTypes.string,
+    style: PropTypes.object,
+  }
+
+  static defaultProps = {
+    count: 1,
+    showCount: true,
+    className: null,
+    style: {},
+  }
+
+  pluralize(singular, plural, count, showCount) {
+    let output = singular;
+
+    if(count !== 1) {
+      output = plural || `${singular}s`;
+    }
+
+    return showCount ? `${count} ${output}` : output
   }
 
   render() {
+    const {
+      singular,
+      plural,
+      count,
+      showCount,
+      className,
+      style,
+    } = this.props;
+
+    const output = this.pluralize(singular, plural, count, showCount);
+
     return (
-      
-    )
+      <span className={className} style={style}>
+        {output}
+      </span>
+    );
   }
 }
 
