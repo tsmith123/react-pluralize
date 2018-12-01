e437bc962862082c9a65ed9392ca1fbae2b64f46
Thomas Smith <tsmith123@users.noreply.github.com>
2018-12-01 12:13:45 +0000

    Add scripts

--- src/Pluralize.js
+++ src/Pluralize.js
@@ -5,11 +5,11 @@
 
 import React from 'react'
 import PropTypes from 'prop-types'
-import { pluralize } from './utils'
+// import { pluralize } from './utils'
 
 const Plural = ({ className, style, ...props }) => (
   <span className={className} style={style}>
-    {pluralize(props)}
+    Test {/* {pluralize(props)} */}
   </span>
 )
 
