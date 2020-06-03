13f1a4754302483c23e1cee0c90992eb9a143e8e
Andi Wilson <pdxbmw@gmail.com>
2020-06-03 09:27:59 -0700

    Add `textOnly` prop render without span

--- src/Pluralize.js
+++ src/Pluralize.js
@@ -3,32 +3,37 @@
  * Tom Smith (https://github.com/tsmith123)
  */
 
-import React from 'react'
-import PropTypes from 'prop-types'
-import { pluralize } from './utils'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pluralize } from './utils';
 
-const Plural = ({ className, style, ...props }) => (
-  <span className={className} style={style}>
-    {pluralize(props)}
-  </span>
-)
+const Plural = ({ className, style, ...props }) =>
+  textOnly ? (
+    pluralize(props)
+  ) : (
+    <span className={className} style={style}>
+      {pluralize(props)}
+    </span>
+  );
 
 Plural.propTypes = {
   singular: PropTypes.string.isRequired,
   plural: PropTypes.string,
   count: PropTypes.number,
   showCount: PropTypes.bool,
+  textOnly: PropTypes.bool,
   className: PropTypes.string,
   style: PropTypes.object,
   zero: PropTypes.string
-}
+};
 
 Plural.defaultProps = {
   count: 1,
   showCount: true,
+  textOny: false,
   className: null,
   style: {},
   zero: null
-}
+};
 
-export default Plural
+export default Plural;
